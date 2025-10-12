9c240b04e801ca5c12ddea5eef5940cc2dc593d6
DEV SUNDRESH <sundhareshks2002@gmail.com>
2025-10-12 08:49:51 -0400

    cloud image show

--- billingproj/urls.py
+++ billingproj/urls.py
@@ -23,7 +23,7 @@ from billapp import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('billapp.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
